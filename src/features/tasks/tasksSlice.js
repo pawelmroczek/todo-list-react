9ed33ad732a0fb5b7c4f651d6611e1adec2dc663
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-06-21 18:32:09 +0200

    new selectors added

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -51,4 +51,15 @@ export const selectIsEveryTasksDone = (state) =>
   selectTasks(state).every(({ done }) => done);
 export const selectAreTasksEmpty = (state) => selectTasks(state).length === 0;
 
+export const getTaskById = (state, taskID) => selectTasks(state).find(({id})=> id === taskID);
+export const selectTasksByQuery = (state, query )=> {
+  const tasks=selectTasks(state);
+
+  if(!query || query.trim() === ""){
+    return tasks;
+  }
+
+  return tasks.filter(({content})=>content.toUpperCase().includes(query.trim().toUpperCase()))
+}
+
 export default tasksSlice.reducer;
