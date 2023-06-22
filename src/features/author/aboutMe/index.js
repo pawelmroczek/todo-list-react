aca8cb33822d4746ebcdb3a4385bc8f0ae8ef5a7
Pawel Mroczek <pawel.mroczekpl@gmail.com>
2023-06-22 08:00:30 +0200

    author page changed

--- src/features/author/aboutMe/index.js
+++ src/features/author/aboutMe/index.js
@@ -1,11 +1,9 @@
 const AboutMe = () => (
   <p>
-    Mam na imię <b>Paweł</b> mam 20 lat i bardzo miło mi Ciebie poznać! Jestem z
-    Bydgoszczy i tu skończyłem szkołe na profilu <em>technik teleinformatyk.</em> Mimo,
-    że szkoła nie należała do najłatwiejszych, to ja wciąż pragnąłem wiedzieć
-    więcej i więcej. Dlatego po ukończeniu wszystkich dodatkowych kursów w
-    szkolę, trafiłem tutaj i wiem, że była to najlepsza decyzja jaką mogłem
-    podjąć.
+    Mam na imię <b>Paweł</b> mam 20 lat i bardzo miło mi Ciebie poznać! Jestem
+    studentem <em>Informatyki </em> na Politechnice Gdańskiej, a programowanie to moja
+    pasja. Poza kodowaniem uwielbiam też uprawiać sport, szczególnie jeździć na
+    snowboardzie i grać w piłkę nożną.
   </p>
 );
 
